3f90fa89d06127fac182178464c631ae1a2ee4bf
Gaston Mollica <gaston.mollica@fishtownanalytics.com>
2022-06-27 00:03:17 +0000

    latest commit

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -7,6 +7,6 @@ WITH orders AS (
     FROM raw.jaffle_shop.orders
 )
 
-SELECT * FROM orders;
+SELECT * FROM orders
 
 /* The only changes this model has made from the initial data is renaming is to order_id
